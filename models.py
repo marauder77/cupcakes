2ad5303ae24924578d38a56d4e76a23c9b190b1e
marauder77 <63757565+marauder77@users.noreply.github.com>
2022-07-28 20:22:26 -0700

    adding other routes and html/css

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ class Cupcake(db.Model):
 
     __tablename__ = "cupcakes"
 
-    id = db.Column(db.Integer, primary_key=True autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     flavor = db.Column(db.Text, nullable = False)
     size = db.Column(db.Text, nullable = False)
     rating = db.Column(db.Float, nullable = False)
@@ -29,6 +29,6 @@ class Cupcake(db.Model):
             "image": self.image,
         }
 
-    def connect_db(app):
-        db.app = app
-        db.init_app(app)
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
